2700e76cb34cccfd4b8eb47d9cc5341833b0ad2e
Lennert Slabbinck <LennertSk@users.noreply.github.com>
2019-12-30 18:37:27 +0100

    Dashboard done

--- iot-project/resources/views/show.blade.php
+++ iot-project/resources/views/show.blade.php
@@ -2,25 +2,58 @@
 
 @section('content')
 
-<div class="container">
-	<div class="buttons">
+<div class="container container-dashboard">
+	<div class="wrapper-main">
+		<div class="dashboard-value">
+			<h1>Dashboard</h1>
+		</div>
 		@foreach($values as $value)
-		<div class="counters">
-			<!-- Knop voor koffie bij te tellen -->
-			<a href="{{ route('addwater') }}">Add water</a><!-- Aantal koffies -->
-			<p>{{ $value->threshold_water }}</p>
-		</div>
-		<div class="counters">
-			<a href="{{ route('waterplants') }}">Add day</a>
-			<p>{{ $value->threshold_plants }}</p>
-		</div>
-		<div class="counters">
-			<a href="{{ route('addcoffee') }}">Add coffee</a>
-			<p>{{ $value->threshold_coffee }}</p>
-		</div>
+			<div class="dashboard-value-water">
+				<p class="value-header">Water Score</p>
+				<p>
+					@if (!is_null($value->value_water))
+					{{ $value->value_water }} / {{ $value->threshold_water }}
+					@else
+					0 / {{ $value->threshold_water }}
+					@endif
+				</p>
+			</div>
+			<div class="dashboard-value-plants"> 
+				<p class="value-header">Plants Score</p>
+				<p>
+					@if (!is_null($value->value_plants))
+					{{ $value->value_plants }} / {{ $value->threshold_plants }}
+					@else
+					0 / {{ $value->threshold_plants }}
+					@endif
+				</p>
+			</div>
+			<div class="dashboard-value-coffee">
+				<p class="value-header">Coffee Score</p>
+				<p>
+					@if (!is_null($value->value_coffee))
+					{{ $value->value_coffee }} / {{ $value->threshold_coffee }}
+					@else
+					0 / {{ $value->threshold_coffee }}
+					@endif
+				</p>
+			</div>
 		@endforeach
+		<div class="dashboard-bnts">
+			<h2 id='bnt-group-header'>Add manually</h2>
+			<ul class='btn-group'>
+				<li><a id='btn-water' href="{{ route('addwater') }}">+</a></li>
+				<li><a id='btn-plants' href="{{ route('waterplants') }}">+</a></li>
+				<li><a id='btn-coffee' href="{{ route('addcoffee') }}">+</a></li>
+			</ul>
+			
+			
+			
+		</div>
 	</div>
 </div>
+
+
 
 
 @endsection
